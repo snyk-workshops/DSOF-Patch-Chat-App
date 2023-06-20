a186a9d8a5b2b09cb212b9270561413632c8755c
Micah Silverman <micah@afitnerd.com>
2023-06-20 08:33:31 -0500

    Added configurable cors definition

--- src/main/java/io/snyk/snyklabs/MultiroomChatApplication.java
+++ src/main/java/io/snyk/snyklabs/MultiroomChatApplication.java
@@ -2,12 +2,18 @@ package io.snyk.snyklabs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vavr.jackson.datatype.VavrModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MultiroomChatApplication {
+
+	@Value("#{ @environment['cors.allowed_domains']}")
+	private String[] corsAllowedDomains;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiroomChatApplication.class, args);
@@ -17,6 +23,16 @@ public class MultiroomChatApplication {
 	public ObjectMapper jacksonBuilder() {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.registerModule(new VavrModule());
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/ws/**").allowedOrigins(corsAllowedDomains);
+			}
+		};
 	}
 
 }
--- src/main/java/io/snyk/snyklabs/app/websocket/WebSocketConfig.java
+++ src/main/java/io/snyk/snyklabs/app/websocket/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package io.snyk.snyklabs.app.websocket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("#{ @environment['stomp.allowed_domains']}")
+    private String[] stompAllowedDomains;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/chat");
@@ -19,8 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000",
-                        "chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam")
+                .setAllowedOrigins(stompAllowedDomains)
                 .withSockJS();
     }
 }
